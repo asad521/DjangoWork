8cc28f40bb92d20bf3cc3f8d0fc8e6e80abb6920
asad521 <masadali@live.co.uk>
2022-08-14 12:55:59 +0500

    per view based cache

--- DjangoCache/DjangoCache/settings.py
+++ DjangoCache/DjangoCache/settings.py
@@ -121,12 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 CACHE_MIDDLEWARE_SECONDS = 30
 # For DB cache
-# CACHES = {
-#     'default':{
-#         'BACKEND':'django.core.cache.backends.db.DatabaseCache',
-#         'LOCATION':'my_cache_table'
-#     }
-# }
+CACHES = {
+    'default':{
+        'BACKEND':'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION':'my_cache_table'
+    }
+}
 # For Filebased cache
 
 # CACHES = {
@@ -135,14 +135,14 @@ CACHE_MIDDLEWARE_SECONDS = 30
 #         'LOCATION':'C:\\Users\\m\\Desktop\\Djanog Geekyshows\\DjangoCache\\DjangoCache\\cachefolder'
 #     }
 # }
-# For Localmemory cache
 
-CACHES = {
-    'default':{
-        'BACKEND':'django.core.cache.backends.locmem.LocMemCache',
-        'LOCATION':'unique-snowflake'
-    }
-}
+# For Localmemory cache
+# CACHES = {
+#     'default':{
+#         'BACKEND':'django.core.cache.backends.locmem.LocMemCache',
+#         'LOCATION':'unique-snowflake'
+#     }
+# }
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- DjangoCache/cacheapp/views.py
+++ DjangoCache/cacheapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from django.views.decorators.cache import cache_page
 # Create your views here.
 
+@cache_page(30)
 def courseView(reqeust):
     return render (reqeust,'course.html')
