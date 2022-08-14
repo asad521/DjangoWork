d296861ceb4e750ee221d0ead3fc216ac0697957
asad521 <masadali@live.co.uk>
2022-08-14 12:58:56 +0500

    per url cache

--- DjangoCache/DjangoCache/urls.py
+++ DjangoCache/DjangoCache/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from cacheapp import views
+from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.courseView),
-]
+    path('', cache_page(30)(views.courseView)),
+] 
