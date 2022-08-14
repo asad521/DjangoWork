b3e8a1cd097d98df8db75536f92af0eca2d5c4db
asad521 <masadali@live.co.uk>
2022-08-14 12:10:02 +0500

    Django per site cache

--- DjangoSessionProject/sessionapp/views.py
+++ DjangoSessionProject/sessionapp/views.py
@@ -3,17 +3,24 @@ from django.shortcuts import render
 
 
 def setSession(request):
-    request.session['password'] = 'Sonamissession'
+    request.session['password'] = 'Sonam_is_session_value'
+    request.session.set_expiry(15) 
+    request.session.set_expiry(0)
     return render(request, 'setsession.html')
 
 
 def getSession(request):
     # sessionCookie =request.session['password']
-    sessionCookie = request.session.get("name", "defaultvalue")
-    return render(request, 'getsession.html', {'sessionCookie': sessionCookie})
+    sessionCookie = request.session.get("password", "defaultvalue")
+    keys = request.session.keys()
+    items = request.session.items()
+    return render(request, 'getsession.html', {'sessionCookie': sessionCookie,'keys':keys,'items':items})
 
 
 def delSession(request):
+    
+    request.session.flush()
+    request.session.clear_expired()
     if "password" in request.session:
         del request.session['password']
     return render(request,'deletesession.html')
