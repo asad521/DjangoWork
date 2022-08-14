4ada6839af9a4225332e260271ee0a5a4ce4b23e
asad521 <masadali@live.co.uk>
2022-08-14 15:18:23 +0500

    template based cache

--- DjangoCache/DjangoCache/urls.py
+++ DjangoCache/DjangoCache/urls.py
@@ -2,9 +2,8 @@
 from django.contrib import admin
 from django.urls import path
 from cacheapp import views
-from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', cache_page(30)(views.courseView)),
+    path('', views.courseView),
 ] 
