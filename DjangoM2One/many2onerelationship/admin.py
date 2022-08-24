6c3a54663082ef4feca8bd580003d713428fc8c7
asad521 <masadali@live.co.uk>
2022-08-25 04:40:15 +0500

    many to one relationship

--- DjangoM2One/many2onerelationship/admin.py
+++ DjangoM2One/many2onerelationship/admin.py
@@ -1,3 +1,8 @@
+from atexit import register
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+@admin.register(Post)
+class Postadmin(admin.ModelAdmin):
+    list_display = ['post_title','post_cat','post_date','user']
--- DjangoM2One/many2onerelationship/models.py
+++ DjangoM2One/many2onerelationship/models.py
@@ -1,3 +1,17 @@
+from operator import mod
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# This is example of one2many relations ship.
+# user can create as many post as he like
+class Post(models.Model):
+    # if user is deleted,all post will be deleted
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    # user cannot be deleted, untill all post are deleted
+    user = models.ForeignKey(User,on_delete=models.PROTECT)
+    # If user is deleted, then post will be remain with user Null
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    post_title = models.CharField(default="None", max_length=70)
+    post_cat = models.CharField(default="None", max_length=70)
+    post_date = models.DateField()
