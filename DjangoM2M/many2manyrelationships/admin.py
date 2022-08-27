bfb4b251837cf04011063d267765d4be1191d371
asad521 <masadali@live.co.uk>
2022-08-27 19:41:08 +0500

    many to many relationship

--- DjangoM2M/many2manyrelationships/admin.py
+++ DjangoM2M/many2manyrelationships/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Songs
 # Register your models here.
+
+
+@admin.register(Songs)
+class SongAdmin(admin.ModelAdmin):
+    list_display =['song_name','song_duration','written_by']
--- DjangoM2M/many2manyrelationships/models.py
+++ DjangoM2M/many2manyrelationships/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+class Songs(models.Model):
+    user = models.ManyToManyField(User)
+    song_name = models.CharField(max_length=30,default='UnKnown')
+    song_duration = models.IntegerField(default=0)
+
+
+    def written_by(self):
+        return ",".join([str(p) for p in self.user.all()])
